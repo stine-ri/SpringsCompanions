aab158a06ec867db155918b52741cfb70ac1f23b
Christine Nyambwari <christinenyambwari@gmail.com>
2025-10-12 05:44:35 -0700

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import VitePluginSitemap  from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
+const routes = [
+  '/',
+  '/about',
+  '/services', 
+  '/contact',
+  '/faq',
+]
+
 export default defineConfig({
   plugins: [
-    react()
-  ,
-    tailwindcss()],
+    react(),
+    tailwindcss(),
+    VitePluginSitemap({
+      hostname: 'https://www.springscompanions.com',
+      dynamicRoutes: routes,
+      generateRobotsTxt: true,
+    }),
+  ],
 })
